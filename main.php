30a28b82bc66d8e6d4004b8cb4de567a0c8bee6f
TyrellMartens <tyrell.martens@uleth.ca>
2022-11-06 13:09:30 -0700

    Basic files and format.Leave main,focus operations

---
+++ main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tydon's Pizzeria<title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <h1> Welcome to Tydon's Pizzeria </h1>
+        <h2> Select from one of the following operations: </h2>
+    </body>
+</html>
