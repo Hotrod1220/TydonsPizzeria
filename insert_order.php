c87febfe1823441dac8ed26c451f1e6078eeeca9
TyrellMartens <tyrell.martens@uleth.ca>
2022-11-08 13:08:52 -0700

    Finished insert, probably lots of errors

--- insert_order.php
+++ insert_order.php
@@ -2,8 +2,6 @@
 <head><title>Tydon's Pizzeria</title></head>
 <body>
 
-
-
 <?php
 if(isset($_COOKIE["username"])){
    
@@ -21,25 +19,27 @@ if(isset($_COOKIE["username"])){
    $result2 = $conn->query($sql2);
    if($result1->num_rows != 0 && $result2->num_rows != 0)
    {
-      //This is where I stopped, after this is her copy and pasted cody ahaha
       echo "Order ID: <input type=text name=\"orderID\" size=8> <br><br>";
-      echo "Contents: <input type=text name=\"contents\" size=20> <br><br>";
-      echo "Status: <input type=text name=\"contents\" size=20> <br><br>";
-      echo "Employee ID: <select name=\"dname\">";
-      echo "Customer ID: <select name=\"dname\">";
+      echo "Contents: <input type=text name=\"contents\" size=40> <br><br>";
+      echo "Status: <input type=text name=\"status\" size=1> <br><br>";
+      echo "Price: <input type=text name=\"price\" size=5> <br><br>";
+      echo "Order Time: <input type=text name=\"orderTime\" size=5> <br><br>";
+      echo "Is it Complete: <input type=checkbox name=\"isComplete\" size=1> <br><br>";
+      echo "Employee ID: <select name=\"empID\"><br><br>";
+      echo "Customer ID: <select name=\"cusID\">";
 
       
-      while($val = $result->fetch_assoc())
+      while($val = $result1->fetch_assoc() && $val2 = $result2->fetch_assoc())
       {
-	 echo "<option value='$val[dname]'>$val[dname]</option>"; 
-	 
+	   echo "<option value='$val[empID]'>$val[empID]</option>"; 
+      echo "<option value='$val2[cusID]'>$val2[cusID]</option>"; 
       }
       echo "</select>"; 
-      echo "<input type=submit name=\"submit\" value=\"Add Course\">"; 
+      echo "<input type=submit name=\"submit\" value=\"Add Order\">"; 
    }
    else
    {
-      echo "<H3>There are no departments in the system! </H3>"; 
+      echo "<H3>There are no customers or employees in the system! </H3>"; 
    }
    
    echo "</form>";
