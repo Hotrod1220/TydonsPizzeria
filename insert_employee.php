433ba8fca2bd03e67c3cb839cfab233a78ecb637
TyrellMartens <tyrell.martens@uleth.ca>
2022-11-10 10:06:54 -0700

    Insert Employee. Browser displayed code.

---
+++ insert_employee.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tydon's Pizzeria - Hire a New Employee</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <h2>Hire a New Employee</h2>
+        <form action="insertemployee.php" method=post>
+            Employee ID: <input type=text name="empID" size=8><br><br>
+            Wage: <input type=text name="wage" size=6><br><br>
+            Position: <input type=text name="position" size=15><br><br>
+            On Shift?: <input type=text name="clockedIn" size=15><br><br>
+            <input type=submit name="submit" value="Insert">
+        </form>     
+    </body>
+</html>
---
+++ insertemployee.php
@@ -0,0 +1,32 @@
+<?php
+require_once '/home/hipt3660/config/mysql_config.php';
+
+if (isset($_COOKIE["username"])) { 
+    $username = $_COOKIE["username"];
+    $password = $_COOKIE["password"];
+    
+    $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+    if($mysqli->connect_errno) {
+        echo "<p>Connection Issue!</p>";
+        exit;
+    }
+
+    $sql = "insert into EMPLOYEE (empID, wage, position, clockedIn) values ('$_POST[empID]','$_POST[wage]','$_POST[position]','$_POST[clockedIn]')";
+    if($conn->query($sql)) {
+        echo "<h3>Employee has been hired.</h3>";
+    }
+    else {
+        $err = $conn->errno; 
+        if($err == 1062)
+        {
+           echo "<p>Employee ID $_POST[empID] already exists.</p>"; 
+        } else {
+           echo "<p>MySQL error code $err </p>";
+        }
+    }
+    echo "<a href=\"main.php\">Return</a> to Home Page.";
+
+} else {
+   echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>"; 
+}
+?>
