d84f5304b34217423a9f2a8f5c0b35a5f4ef6ff8
Tyler Hippard <jellyblade@gmail.com>
2022-11-10 04:54:59 -0700

    Added a php page to force update the website from the github using a shell script I made. Hope it works.

---
+++ update_website.php
@@ -0,0 +1,11 @@
+<?php
+  echo "Press the button to update the website from github.";
+  echo "<form method="post">
+          <input type="submit" name="update" value="Update"/>
+        </form>";
+  if (array_key_exists('update', $_POST)) {
+    echo "Updating website...";
+    $output = shell_exec('/home/hipt3660/update_website');
+    echo "<pre>$output</pre>";
+  }
+?>
