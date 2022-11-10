90584f585341505f70246e8e98ffc6396843da29
Tyler Hippard <jellyblade@gmail.com>
2022-11-10 05:45:18 -0700

    Fixes for update script

--- update_website.php
+++ update_website.php
@@ -5,7 +5,9 @@
         </form>';
   if (array_key_exists('update', $_POST)) {
     echo 'Updating website...';
-    $output = shell_exec('/home/hipt3660/update_website');
+    $output = shell_exec('/home/hipt3660/update_website 2>&1');
+    shell_exec('chmod -R 777 /home/hipt3660/public_html/*');
     echo "<pre>$output</pre>";
+    echo 'Updated!';
   }
 ?>
