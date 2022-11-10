c661625f6ccff6595a58794fe0a6b5b742f95644
Tyler Hippard <jellyblade@gmail.com>
2022-11-10 06:12:18 -0700

    Added some buttons to the home page

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-
+<?php
+  echo '<html>
+        <head>
+          <title>Tydon\'s Pizzeria</title>
+        </head>
+        <body>
+  ';
+  echo '<h1>Welcome to Tydon\'s Pizzeria</h1>\n
+        <button onclick="window.location.href = \'insert_order.php\'">Order a pizza!</button><br><br>
+        <button onclick="window.location.href = \'insert_employee.php\'">Hire employee</button><br><br>
+        <button onclick="window.location.href = \'update_employee.php\'">Manage employees</button><br><br>
+        More buttons coming soon...<br><br>
+        <button onclick="window.location.href = \'update_website.php\'">DEV: update website page</button>
+  ';
+?>
