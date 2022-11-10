6ed7954cb3b6465711e420800aa172de022bc782
TyrellMartens <tyrell.martens@uleth.ca>
2022-11-10 10:12:00 -0700

    Woops forgot to delete that

--- insertemployee.php
+++ insertemployee.php
@@ -1,11 +1,6 @@
 <?php
 require_once '/home/hipt3660/config/mysql_config.php';
     
-$conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
-if($mysqli->connect_errno) {
-    echo "<p>Connection Issue!</p>";
-    exit;
-}
 $sql = "insert into EMPLOYEE (empID, wage, position, clockedIn) values ('$_POST[empID]','$_POST[wage]','$_POST[position]','$_POST[clockedIn]')";
 if($conn->query($sql)) {
     echo "<h3>Employee has been hired.</h3>";
