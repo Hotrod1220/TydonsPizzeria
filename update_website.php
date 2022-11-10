87cbad68e256f5d763fcfa0208ff9d9cbf32520d
Tyler Hippard <jellyblade@gmail.com>
2022-11-10 04:58:04 -0700

    Mm, forgot how strings work in PHP :P

--- update_website.php
+++ update_website.php
@@ -1,10 +1,10 @@
 <?php
-  echo "Press the button to update the website from github.";
-  echo "<form method="post">
+  echo 'Press the button to update the website from github.';
+  echo '<form method="post">
           <input type="submit" name="update" value="Update"/>
-        </form>";
+        </form>';
   if (array_key_exists('update', $_POST)) {
-    echo "Updating website...";
+    echo 'Updating website...';
     $output = shell_exec('/home/hipt3660/update_website');
     echo "<pre>$output</pre>";
   }
