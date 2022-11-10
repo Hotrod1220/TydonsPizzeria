519d9d88d2fecfe0a9f1baa6565f5769096ec685
Tyler Hippard <jellyblade@gmail.com>
2022-11-10 08:25:00 -0700

    More fixing

--- update_employee.php
+++ update_employee.php
@@ -36,7 +36,7 @@
               <td>{$row['position']}</td>
               <td>{$row['clockedIn']}</td>
               <td>
-                <button onclick=\"window.location.href = 'insert_employe.php?empID={$row['empID']}'\">Edit</button>
+                <button type='button' onclick=\"window.location.href = 'insert_employee.php?empID={$row['empID']}'\">Edit</button>
               </td>
               <td><input type='checkbox' name='fire[{$row['empID']}]'></td>
             </tr>
