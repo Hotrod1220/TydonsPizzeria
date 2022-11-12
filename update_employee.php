fe91c209fd008727387a3c4a95cde0d7e84f3512
Tyler Hippard <jellyblade@gmail.com>
2022-11-11 21:05:38 -0700

    Fixed syntax errors

--- update_employee.php
+++ update_employee.php
@@ -2,16 +2,14 @@
 if (isset($_POST['empID'])) {
   require_once '/home/hipt3660/config/mysql_config.php';
 
-  $sql = "update EMPLOYEE set
-    name = '$_POST['name'],
-    wage = '$_POST['wage'],
-    position = '$_POST['position'],
-    clockedIn = '$_POST['clockedIn'],
-    where empID = '$_POST['empID'];"
-  ;
+  $sql = "update EMPLOYEE set name = '{$_POST['name']}',
+    wage = {$_POST['wage']},
+    position = '{$_POST['position']}',
+    clockedIn = '{$_POST['clockedIn']}',
+    where empID = {$_POST['empID']};";
   try {
     $conn->query($sql);
-    echo "Information for $_POST['name'] updated successfully.<br><br>";
+    echo "Information for {$_POST['name']} updated successfully.<br><br>";
     echo "<a href=\"main.php\">Return</a> to Home Page.";
     exit();
   } catch (Exception $e) {
@@ -33,21 +31,21 @@ if (isset($_POST['empID'])) {
   <body>
     <?php
       require_once '/home/hipt3660/config/mysql_config.php';
-      $sql = "select * from EMPLOYEE where empID = $_GET['empID']";
+      $sql = "select * from EMPLOYEE where empID = {$_GET['empID']}";
       $result = $conn->query($sql);
 
       if ($result->num_rows != 0) {
-        echo "<h2>Update Information for $result['name']</h2>;"
+        echo "<h2>Update Information for {$result['name']}</h2>;";
         echo "<form action='', method='post'>";
-        echo "Name: <input type=text name='name' value='$result['name']' size=20><br><br>";
-        echo "Wage: <input type=text name='wage' value='$result['wage']' size=6><br><br>";
-        echo "Position: <input type=text name='position' value='$result['position']' size=15><br><br>";
-        echo "On Shift?: <input type=checkbox name='clockedIn' value=$result['clockedIn']><br><br>";
-        echo "<input type=hidden name='empID' value='$_GET[empID]'>";
+        echo "Name: <input type=text name='name' value='{$result['name']}' size=20><br><br>";
+        echo "Wage: <input type=text name='wage' value={$result['wage']} size=6><br><br>";
+        echo "Position: <input type=text name='position' value='{$result['position']}' size=15><br><br>";
+        echo "On Shift?: <input type=checkbox name='clockedIn' value={$result['clockedIn']}><br><br>";
+        echo "<input type=hidden name='empID' value='{$_GET['empID']}'>";
         echo "<input type=submit name='submit' value='Update'>";
         echo "</form>";
       } else {
-        echo "no employee with ID $_GET['empID'] :(";
+        echo "no employee with ID {$_GET['empID']} :(";
       }
     ?>
   </body>
