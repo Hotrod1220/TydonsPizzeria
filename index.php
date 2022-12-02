04aec47e9a79ad5399ba32a98baad27b9c58c2a2
Landon <landonconstantin@gmail.com>
2022-12-02 16:52:41 -0700

    added order page to index

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <button onclick="window.location.href = \'manage_customer.php\'">Manage customers</button><br><br>
         <button onclick="window.location.href = \'insert_employee.php\'">Hire employee</button><br><br>
         <button onclick="window.location.href = \'manage_employee.php\'">Manage employees</button><br><br>
+        <button onclick="window.location.href = \'insert_order.php\'">Place an order</button><br><br>
         More buttons coming soon...<br><br>
         <button onclick="window.location.href = \'update_website.php\'">DEV: update website page</button>
   ';
