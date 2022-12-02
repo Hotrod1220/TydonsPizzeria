274f098be64a8480fa3632e306c4d5886b90b2ce
TyrellMartens <tyrell.martens@uleth.ca>
2022-12-02 14:17:54 -0700

    create table sql statements

---
+++ createTables.sql
@@ -0,0 +1,49 @@
+create table EMPLOYEE
+(
+    empID int primary key auto_increment, 
+    name varchar(50), 
+    wage float, 
+    position varchar(30),
+    clockedIn boolean
+);
+
+create table CUSTOMER
+(
+    custID int primary key auto_increment, 
+    name varchar(50), 
+    address varchar(50), 
+    email varchar(50), 
+    phoneNum varchar(20)
+);
+
+create table MENU
+(
+    itemID int primary key auto_increment, 
+    itemName varchar(50), 
+    itemPrice float, 
+    isVegan boolean, 
+    stock int
+);
+
+create table ORDER
+(
+    orderID int primary key auto_increment, 
+    content varchar(100), 
+    status varchar(50), 
+    price float, 
+    orderTime int, 
+    isComplete boolean, 
+    foreign key (empID) references EMPLOYEE (empID),
+    -- Do we need a on delete ____ on update ____
+    foreign key (custID) references CUSTOMER (custID)    
+    -- Do we need a on delete ____ on update ____
+);
+
+create table CONTAINS
+(
+    foreign key (itemID) references MENU (itemID)
+    on delete cascade on update cascade,
+    foreign key (orderID) references ORDER (orderID)
+    on delete cascade on update cascade,
+    quantity int --int? 
+);
