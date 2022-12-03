8f34df796e7176bc9698f0c71c863e9e75664009
Landon <landonconstantin@gmail.com>
2022-12-02 17:21:07 -0700

    idk why I thought sessions would even help

--- insert_order.php
+++ insert_order.php
@@ -2,27 +2,20 @@
     require_once '/home/hipt3660/config/mysql_config.php';
     session_start(); 
     $sql = "SELECT * FROM MENU";
-    echo '<table border="0" cellspacing="2" cellpadding="2"> 
-      <tr> 
-          <td> <font face="Arial">Value1</font> </td> 
-          <td> <font face="Arial">Value2</font> </td> 
-          <td> <font face="Arial">Value3</font> </td> 
-          <td> <font face="Arial">Value4</font> </td> 
-          <td> <font face="Arial">Value5</font> </td> 
-      </tr>';
-    if($conn->query($sql)) {
-        while($row = $conn->fetch_assoc()) {
+    $result = $conn->query($sql);
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
             $field1name = $row["itemID"];
             $field2name = $row["itemName"];
             $field3name = $row["itemPrice"];
             $field4name = $row["isVegan"];
             $field5name = $row["stock"];
             echo '<tr> 
-                      <td>'.$field1name.'</td> 
-                      <td>'.$field2name.'</td> 
-                      <td>'.$field3name.'</td> 
-                      <td>'.$field4name.'</td> 
-                      <td>'.$field5name.'</td> 
+                      <td>'. $field1name.'</td> 
+                      <td>'. $field2name.'</td> 
+                      <td>'. $field3name.'</td> 
+                      <td>'. $field4name.'</td> 
+                      <td>'. $field5name.'</td> 
                   </tr>';
         }
         echo "<h3>Order up!</h3>";
