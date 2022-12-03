2a1b46577dce81f8c71c4e0a6109d9700fe135ad
Landon <landonconstantin@gmail.com>
2022-12-02 17:30:35 -0700

    fixed vegan, further table formatting

--- insert_order.php
+++ insert_order.php
@@ -1,7 +1,13 @@
 <?php
     require_once '/home/hipt3660/config/mysql_config.php';
     session_start(); 
-    echo '<table>';
+    echo '<table> <tr> 
+    <td> ID </td> 
+    <td> Name </td> 
+    <td> Price </td> 
+    <td> Vegan </td> 
+    <td> Stock </td> 
+</tr>';
     $sql = "SELECT * FROM MENU";
     $result = $conn->query($sql);
     if($result->num_rows > 0) {
@@ -11,11 +17,18 @@
             $field3name = $row["itemPrice"];
             $field4name = $row["isVegan"];
             $field5name = $row["stock"];
+
+            if($field4name) {
+                $vegan = "Yes";
+            } else {
+                $vegan = "No";
+            }
+            
             echo '<tr> 
                       <td>'. $field1name.'</td> 
                       <td>'. $field2name.'</td> 
                       <td>'. $field3name.'</td> 
-                      <td>'. $field4name.'</td> 
+                      <td>'. $vegan. '</td> 
                       <td>'. $field5name.'</td> 
                   </tr> <br>';
         }
