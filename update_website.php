0c5b7d73f2299cedf5f109b9f4e91d9d1feb420e
Tyler Hippard <jellyblade@gmail.com>
2022-12-03 18:06:04 -0700

    Fix for cron being silly

--- update_website.php
+++ update_website.php
@@ -5,7 +5,7 @@
         </form>';
   if (array_key_exists('update', $_POST)) {
     echo 'Updating website...';
-    $output = shell_exec('/home/hipt3660/update_website 2>&1');
+    $output = shell_exec('/home/hipt3660/update_website1 2>&1');
     shell_exec('chmod -R 777 /home/hipt3660/public_html/*');
     echo "<pre>$output</pre>";
     echo 'Updated!';
